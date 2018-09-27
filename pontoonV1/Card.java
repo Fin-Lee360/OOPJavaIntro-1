acad962f8ebb951a8e694c06c5566ebca28c31fe
Campbell37 <43272940+Campbell37@users.noreply.github.com>
2018-09-27 12:47:26 +0100

    Add files via upload

--- pontoonV1/Card.java
+++ pontoonV1/Card.java
@@ -1,3 +1,8 @@
+/*
+ * Sol Campbell 27/09/2018
+ * Version 1.1 of pontoon
+ */
+
 package pontoonV1;
 
 import java.util.Scanner;
@@ -6,26 +11,24 @@ public class Card
 {
 	Scanner kboard = new Scanner(System.in);
 	
-	private int card1;
+	private int card1;			// declarations
 	private int card2;
 	private int total;
 	private int cardNew;
 	private int dealerCard1;
 	private int dealerCard2;
 	private int dealerTotal;
-//	private String suite;
-//	private boolean drawn;
 	private String choice;
 	
 	
-	public Card()
+	public Card() // constructor
 	{
 		this.getcard1Value();
 		this.getcard2Value();
 		this.getDealerCard();
 	}
 	
-	public void display()
+	public void display() // displays the game in progress
 	{
 		System.out.println("Your cards are " +card1+ " and " +card2+".");
 		total = card1 + card2;
@@ -44,7 +47,7 @@ public class Card
 				{
 					System.out.println("You cannot take a card if your total is above 21.");
 				}
-			}while ((choice.equalsIgnoreCase("y")) && total<22);
+			}while ((choice.equalsIgnoreCase("y")) && total<22); // runs the loop as long as the player is below 22 or has chosen to stay on their number
 		}
 		if (choice.equalsIgnoreCase("n") || total>21)
 		{
@@ -53,61 +56,63 @@ public class Card
 				do
 				{
 					this.getDealerTotal();
-				}while ((dealerTotal<total) && (total<22));
+				}while ((dealerTotal<total) && (total<22)); // lets the dealer 'draw' until they have matched or beat the player, or gone bust
 			}
 			
-			this.getCheckWin();
+			this.getCheckWin(); // checks the win condition based on the players and dealers cards
 		}
 	}
 	
-	public void getcard1Value()
+	public void getcard1Value() // generates 1st player card
 	{
 		card1 = (int)(Math.random()*10)+1;
 	}
 	
-	public void getcard2Value()
+	public void getcard2Value() // generates 2nd player card
 	{
 		card2 = (int)(Math.random()*10)+1;
 	}
 	
-	public void getcardNewValue()
+	public void getcardNewValue() // generates a new card if the player wants one
 	{
 		cardNew = (int)(Math.random()*10)+1;
 		System.out.println("Your new card is " +cardNew);
 	}	
 	
-	public void getTotal()
+	public void getTotal() // finds the total for the player
 	{
 		total = total + cardNew;
 		System.out.println("Your total is " +total+".");
 	}
 	
-	public void getDealerCard()
+	public void getDealerCard() // finds the dealers original hand
 	{
 		dealerCard1 = (int)(Math.random()*10)+1;
 		dealerCard2 = (int)(Math.random()*10)+1;
 		dealerTotal = dealerCard1 + dealerCard2;
 	}
 	
-	public void getDealerTotal()
+	public void getDealerTotal() // 'draws' a new card for the dealer
 	{
 		int dealernewCard = (int)(Math.random()*10)+1;
 		dealerTotal = dealerTotal + dealernewCard;
-		System.out.println(dealernewCard);
-		System.out.println(dealerTotal);
 	}
 	
-	public void getCheckWin()
+	public void getCheckWin() // determines who wins the game
 	{
 		if (total>21)
 		{
 			System.out.println("You are bust. You lose.");
 		}
+		else if (total == dealerTotal)
+		{
+			System.out.println("You both have the same card. Dealer wins on a draw.");
+		}
 		else if ((total>21) && dealerTotal>21)
 		{
 			System.out.println("you are both bust. Dealer wins on a draw.");
 		}
-		else if ((total<dealerTotal) && (dealerTotal < 21))
+		else if ((total<dealerTotal) && (dealerTotal < 22))
 		{
 			System.out.println("Dealer has " +dealerTotal+ ". You lose.");
 		}
--- pontoonV1/Game.java
+++ pontoonV1/Game.java
@@ -1,12 +1,14 @@
+/*
+ * Sol Campbell 27/09/2018
+ * Version 1.1 of pontoon
+ */
 package pontoonV1;
 
 public class Game {
 
-	public static void main(String[] args) 
+	public static void main(String[] args) // main where Card class is run
 	{
 		Card Player1 = new Card();
 		Player1.display();
-
 	}
-
 }
